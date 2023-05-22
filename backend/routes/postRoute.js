2e3ee54e4eaeecd9e27c4ea44ad0864b4fe87cfb
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-05-22 17:35:50 +0530

    fnfnf

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,17 +1,25 @@
-const express=require("express")
-const { getPost, postComments, postLikes, newPost, removeComment, removeLike, getSinglePost } = require("../controller/postController")
-const { authorization } = require("../middlewares/authorization")
-const postRoute=express.Router()
+const express = require("express");
+const {
+  getPost,
+  postComments,
+  postLikes,
+  newPost,
+  removeComment,
+  removeLike,
+  getSinglePost,
+} = require("../controller/postController");
+const { authorization } = require("../middlewares/authorization");
+const postRoute = express.Router();
 
-postRoute.use(authorization)
-postRoute.get("/posts",getPost)
-postRoute.get("/posts/:id",getSinglePost)
-postRoute.post("/posts",newPost)
-postRoute.post("/comments",postComments)
-postRoute.post("/likes",postLikes)
-postRoute.post("/comments/:postId/:commentId",removeComment)
-postRoute.post("/likes/:postId",removeLike)
+postRoute.use(authorization);
+postRoute.get("/posts", getPost);
+postRoute.get("/posts/:id", getSinglePost);
+postRoute.post("/posts", newPost);
+postRoute.post("/comments", postComments);
+postRoute.post("/likes", postLikes);
+postRoute.post("/comments/:postId/:commentId", removeComment);
+postRoute.post("/likes/:postId", removeLike);
 
-module.exports={
-    postRoute
-}
+module.exports = {
+  postRoute,
+};
