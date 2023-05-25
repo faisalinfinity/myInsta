d132f1618c0c62b2379aeb3a1376472c10e13a7a
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-05-25 22:31:57 +0530

    create post modal

---
+++ frontend/src/components/CreatePost.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const CreatePost = () => {
+    const [show,setShow]=useState(false)
+  return (
+   <>
+    
+   </>
+  )
+}
+
+export default CreatePost
