676906da361a4da7b26fc3b39faf9b6e97a064a4
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-06-05 01:55:22 +0530

    fix

--- frontend/src/redux/authReducer/types.js
+++ frontend/src/redux/authReducer/types.js
@@ -1,5 +1,6 @@
 export const LOGIN_REQ="login/req"
 export const LOGIN_SUCCESS="login/success"
 export const LOGIN_FAIL="login/fail"
-export const LOGOUT="logout"
+export const LOGOUT="logout" 
 
+ 
