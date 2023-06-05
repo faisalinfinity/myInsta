dc41b308a0f3fff8bfd6d709e5c2cef51e32d3ad
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-06-06 00:51:55 +0530

    authaction

---
+++ frontend/src/redux/authReducer/action.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { BASE_URL } from '../../constants/Baseurl'
+export const registerAction=(formData)=>{
+    return axios.post(`${BASE_URL}/register`,formData)
+    .then((res)=>res)
+    .catch((err)=>err)
+}
+
+export const loginAction=()=>{
+     
+}
