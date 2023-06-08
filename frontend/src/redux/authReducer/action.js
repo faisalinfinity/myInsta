858fe844a0c04d7a67f0e2a3782c5d80ee821ba9
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-06-08 23:49:37 +0530

    loginAction

--- frontend/src/redux/authReducer/action.js
+++ frontend/src/redux/authReducer/action.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { BASE_URL } from '../../constants/Baseurl'
+import { LOGIN_SUCCESS } from './types'
 export const registerAction=(formData)=>{
     return axios.post(`${BASE_URL}/register`,formData)
     .then((res)=>res)
@@ -8,4 +9,5 @@ export const registerAction=(formData)=>{
 
 export const loginAction=(formData)=>(dispatch)=>{
      axios.post(`${BASE_URL}/register`,formData)
+     .then((res)=>dispatch({type:LOGIN_SUCCESS,payload:res.data}))
 }
